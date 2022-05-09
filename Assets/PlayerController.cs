26143bff476b7522b07b8ff382e99f2ca17f0c14
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-05-09 17:27:33 +0530

    playerController added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal")>0){
+            rigidBody2D.velocity = new Vector2(speed,0f);
+        }else if(Input.GetAxis("Horizontal")<0){
+            rigidBody2D.velocity = new Vector2(-speed,0f);
+
+        }else if(Input.GetAxis("Vertical")<0){
+            rigidBody2D.velocity = new Vector2(0f,-speed);
+
+        }else if(Input.GetAxis("Vertical")>0){
+            rigidBody2D.velocity = new Vector2(0f,speed);
+
+        }else if(Input.GetAxis("Horizontal")==0 || Input.GetAxis("Vertical")==0){
+            rigidBody2D.velocity = new Vector2(0f,0f);
+
+        }
+    
+    }
+}

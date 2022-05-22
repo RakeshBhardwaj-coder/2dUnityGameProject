0879f040af2327718db4486e988a51d8a14d5037
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-05-22 15:30:51 +0530

    nothing changed.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,40 +6,45 @@ public class EnemyMovement : MonoBehaviour
 {
     public GameObject pointB;
     public GameObject pointA;
-    GameObject enemy;
+    public GameObject player;
     public float maxSpeed = 3;
     public bool keyHit = false;
     public bool keyHit2 = false;
-    public bool keyHit3 = false;
+    bool isGameOver = true;
     // Start is called before the first frame update
     void Start()
     {
-    gameObject.transform.position = pointA.transform.position;
+        gameObject.transform.position = pointA.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (gameObject.transform.position == pointA.transform.position)
+        if (gameObject.transform.position == pointA.transform.position)
         {
             keyHit = true;
 
         }
         if (keyHit == true)
         {
-            keyHit2 = false;
+            // keyHit2 = false;
             var change = maxSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, pointB.transform.position, change);
         }
         if (gameObject.transform.position == pointB.transform.position)
         {
-            keyHit2 = true;
-        }
-        if (keyHit2 == true)
-        {
+            // keyHit2 = true;
             keyHit = false;
+        }
+        if (keyHit == false)
+        {
+            // keyHit = false;
             var change2 = maxSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, pointA.transform.position, change2);
         }
-       }
+
+        if(isGameOver){
+            transform.position = new Vector3(0,player.transform.position.y,0);
+            }
     }
+}
